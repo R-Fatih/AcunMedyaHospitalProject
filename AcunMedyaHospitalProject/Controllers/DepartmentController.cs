ce3c5fc516e3fd465337c83d5e1bfd446994e075
RFatih <39864827+R-Fatih@users.noreply.github.com>
2025-01-30 21:55:46 +0300

    up

--- AcunMedyaHospitalProject/Controllers/DepartmentController.cs
+++ AcunMedyaHospitalProject/Controllers/DepartmentController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AcunMedyaHospitalProject.Context;
+using AcunMedyaHospitalProject.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,27 @@ namespace AcunMedyaHospitalProject.Controllers
 {
     public class DepartmentController : Controller
     {
-        // GET: Department
+        private readonly AppDbContext db = new AppDbContext();
         public ActionResult Index()
         {
+            var departments = db.Departments.ToList();
+            return View(departments);
+        }
+        [HttpGet]
+        public ActionResult CreateDepartment()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult CreateDepartment(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Departments.Add(department);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Department");
+            }
+            return View(department);
         }
     }
 }
